4bcf468cdfd0e88d589f74b9f23f95816c9029cf
andrescarlos2211 <andrescarlos2211@gmail.com>
2023-01-17 12:07:59 -0300

    Finaliza ejercicio 3

---
+++ ejercicio_3.js
@@ -0,0 +1,7 @@
+while((isNaN(x))||x>100 ){
+    var x = parseInt(prompt('Ingresar un numero entero menor que 100'));
+    }
+for (let i=x;i>0;i--){
+    document.write(i)
+    document.write("<br>")
+}
